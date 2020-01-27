017bb1ff0740d6c3ad717280426f4b876633513e
appear <appear.ko@gmail.com>
2020-01-27 19:10:58 +0900

    :zap: ADD: storybook 을 추가합니다

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -1,3 +1,3 @@
-export { default as Container } from './atom/container'
+export * from './atom/container'
 export { default as ThemeProvider } from './theme-provider'
 export { default as GlobalStyle } from './global-style'
